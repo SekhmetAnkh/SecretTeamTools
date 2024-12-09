8dc960615f67a1c25fc3b01f67bbc53fe4811284
SekhmetAnkh <contact.theankh@gmail.com>
2024-12-09 16:41:51 +0200

    Dark Mode Changes

--- load-navbar.js
+++ load-navbar.js
@@ -7,7 +7,13 @@ function loadNavbar() {
             if (darkModeToggle) {
                 darkModeToggle.addEventListener('click', function() {
                     document.body.classList.toggle('dark-mode');
+                    localStorage.setItem('theme', document.body.classList.contains('dark-mode') ? 'dark' : 'light');
                 });
+
+                // Set initial theme based on localStorage
+                if (localStorage.getItem('theme') === 'dark') {
+                    document.body.classList.add('dark-mode');
+                }
             }
         })
         .catch(error => console.error('Error loading navbar:', error));
