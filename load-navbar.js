2386172f53fd88a99cca0ed119ad3d83f28e19ca
SekhmetAnkh <contact.theankh@gmail.com>
2024-12-09 18:33:01 +0200

    Update

--- load-navbar.js
+++ load-navbar.js
@@ -3,24 +3,8 @@ function loadNavbar() {
         .then(response => response.text())
         .then(data => {
             document.getElementById('navbar-placeholder').innerHTML = data;
-            const darkModeToggle = document.getElementById('darkModeToggle');
-            if (darkModeToggle) {
-                darkModeToggle.addEventListener('click', function() {
-                    const isDarkMode = document.documentElement.getAttribute('data-theme') === 'dark';
-                    if (isDarkMode) {
-                        document.documentElement.removeAttribute('data-theme');
-                        localStorage.setItem('theme', 'light');
-                    } else {
-                        document.documentElement.setAttribute('data-theme', 'dark');
-                        localStorage.setItem('theme', 'dark');
-                    }
-                });
-
-                // Set initial theme based on localStorage
-                if (localStorage.getItem('theme') === 'dark') {
-                    document.documentElement.setAttribute('data-theme', 'dark');
-                }
-            }
         })
         .catch(error => console.error('Error loading navbar:', error));
 }
+
+document.addEventListener('DOMContentLoaded', loadNavbar);
